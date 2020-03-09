84042435f4ac46a6475ad1eb6c6e38f2599557ec
wanbanka <hurt.stress@gmail.com>
2020-03-09 13:07:46 +0100

    Bonjour

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {BooksService} from '../services/books.service';
+import {Subscription} from 'rxjs';
+import {Book} from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private bookService: BooksService) { }
+    
+    public booksSubscription: Subscription;
+    public books: Book[] = [];
 
   ngOnInit() {
+      this.booksSubscription = this.bookService.booksSubject.subscribe(
+      
+          (books) => {
+              this.books = books;
+          }
+          
+      );
+      
+      this.bookService.emitBookSubject();
   }
+    
+    ngOnDestroy(){
+        this.booksSubscription.unsubscribe();
+    }
 
 }
