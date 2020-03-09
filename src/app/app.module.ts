0ce3d1df90c5137c030ef766dec2389230e3d845
wanbanka <hurt.stress@gmail.com>
2020-03-09 11:30:02 +0100

    Premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import {AuthGuardService} from './services/auth-guard.service';
+import {AuthService} from './services/auth.service';
+import {BooksService} from './services/books.service';
+
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    BookFormComponent,
+    HeaderComponent,
+    SingleBookComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      HttpClientModule
   ],
-  providers: [],
+  providers: [
+      AuthGuardService,
+      AuthService,
+      BooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
